da7aaa164ff10bfe73be3af5e897b67875e0f195
Liubov Butorina <liubovbutorina7@yandex.ru>
2025-08-17 22:08:59 +0300

    added goToSectionBunsTest & goToSectionSaucesTest

--- src/main/java/ru/practikum/pages/MainPageConstructor.java
+++ src/main/java/ru/practikum/pages/MainPageConstructor.java
@@ -3,17 +3,24 @@ package ru.practikum.pages;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import ru.practikum.util.Constants;
 
 import java.time.Duration;
 
+import static org.junit.Assert.assertTrue;
+
 public class MainPageConstructor {
     protected final WebDriver driver;
+
     private final By loginAccountButton = By.xpath(".//button[text() = 'Войти в аккаунт']");
     private final By personalAccountButton = By.xpath(".//p[text() = 'Личный Кабинет']");
     private final By sectionBurgers = By.cssSelector(".BurgerIngredients_ingredients__1N8v2");
+    private final By tabBuns = By.xpath(".//span[text() = 'Булки']/parent::div");
+    private final By tabSauces = By.xpath(".//span[text() = 'Соусы']/parent::div");
+    private final By tabFillings = By.xpath(".//span[text() = 'Начинки']/parent::div");
 
     public MainPageConstructor(WebDriver driver) {
         this.driver = driver;
@@ -34,12 +41,74 @@ public class MainPageConstructor {
         driver.findElement(personalAccountButton).click();
     }
 
+    @Step("Click on Tab Buns in Constructor")
+    public void clickTabBuns() {
+        driver.findElement(tabBuns).click();
+    }
+
+    @Step("Click on Tab Sauces in Constructor")
+    public void clickTabSauces() {
+        driver.findElement(tabSauces).click();
+    }
+
+    @Step("Click on Tab Buns in Constructor")
+    public void clickTabFillings() {
+        driver.findElement(tabFillings).click();
+    }
+
+    @Step("Check Tab Buns Activity")
+    public void checkIfTabBunsIsActive() {
+        WebElement element = driver.findElement(tabBuns);
+        String attribute = element.getAttribute("class");
+        assertTrue("Вкладка 'Булки' неактивна", attribute.contains("tab_tab_type_current__2BEPc"));
+    }
+
+    @Step("Check Tab Sauces Activity")
+    public void checkIfTabSaucesIsActive() {
+        WebElement element = driver.findElement(tabSauces);
+        String attribute = element.getAttribute("class");
+        assertTrue("Вкладка 'Соусы' неактивна", attribute.contains("tab_tab_type_current__2BEPc"));
+    }
+
+    @Step("Check Tab Fillings Activity")
+    public void checkIfTabFillingsIsActive() {
+        WebElement element = driver.findElement(tabFillings);
+        String attribute = element.getAttribute("class");
+        assertTrue("Вкладка 'Начинки' неактивна", attribute.contains("tab_tab_type_current__2BEPc"));
+    }
+
+    @Step("Wait until Constructor will be loaded")
+    public void waitLoadingConstructor() {
+        new WebDriverWait(driver, Duration.ofSeconds(Constants.WAITING_SECONDS))
+                .until(ExpectedConditions.and(
+                        ExpectedConditions.elementToBeClickable(sectionBurgers),
+                        ExpectedConditions.visibilityOfElementLocated(sectionBurgers)));
+    }
+
+    @Step("Wait until tab Buns will be clickable")
+    public void waitLoadingTabBuns() {
+        new WebDriverWait(driver, Duration.ofSeconds(Constants.WAITING_SECONDS))
+                .until(ExpectedConditions.and(
+                        ExpectedConditions.elementToBeClickable(tabBuns),
+                        ExpectedConditions.visibilityOfElementLocated(tabBuns)));
+    }
+
+    @Step("Wait until tab Sauces will be clickable")
+    public void waitLoadingTabSauces() {
+        new WebDriverWait(driver, Duration.ofSeconds(Constants.WAITING_SECONDS))
+                .until(ExpectedConditions.and(
+                        ExpectedConditions.elementToBeClickable(tabSauces),
+                        ExpectedConditions.visibilityOfElementLocated(tabSauces)));
+    }
+
     @Step("Check is Main Page (Constructor) displayed")
     public void checkMainPageIsDisplayed() {
         try {
-            new WebDriverWait(driver, Duration.ofSeconds(Constants.WAITING_SECONDS)).until(ExpectedConditions.visibilityOfElementLocated(sectionBurgers));
+            //new WebDriverWait(driver, Duration.ofSeconds(Constants.WAITING_SECONDS)).until(ExpectedConditions.visibilityOfElementLocated(sectionBurgers));
+            waitLoadingConstructor();
         } catch (Exception e) {
             throw new RuntimeException("Конструктор не найден на странице: " + e.getMessage());
         }
     }
+
 }
--- src/test/java/ru/practikum/tests/ConstructorTests.java
+++ src/test/java/ru/practikum/tests/ConstructorTests.java
@@ -1,4 +1,51 @@
 package ru.practikum.tests;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import ru.practikum.pages.MainPageConstructor;
+
 public class ConstructorTests {
+    @Rule
+    public DriverFactory driverFactory = new DriverFactory();
+    WebDriver driver;
+
+    @Before
+    public void setUp() {
+        driver = driverFactory.getDriver();
+    }
+
+    @Test
+    @DisplayName("Go to section 'Buns' in Constructor")
+    @Description("Test go to section 'Buns' via click on tab Buns")
+    public void goToSectionBunsTest() throws InterruptedException {
+        MainPageConstructor mainPageObj = new MainPageConstructor(driver);
+        mainPageObj.openMainPageConstructor();
+        mainPageObj.waitLoadingConstructor();
+        mainPageObj.waitLoadingTabSauces();
+        mainPageObj.clickTabSauces();
+        mainPageObj.waitLoadingTabBuns();
+        mainPageObj.checkIfTabSaucesIsActive();
+        mainPageObj.clickTabBuns();
+        mainPageObj.waitLoadingTabSauces();
+        mainPageObj.checkIfTabBunsIsActive();
+    }
+
+    @Test
+    @DisplayName("Go to section 'Sauces' in Constructor")
+    @Description("Test go to section 'Sauces' via click on tab Buns")
+    public void goToSectionSaucesTest() throws InterruptedException {
+        MainPageConstructor mainPageObj = new MainPageConstructor(driver);
+        mainPageObj.openMainPageConstructor();
+        mainPageObj.waitLoadingConstructor();
+        mainPageObj.checkIfTabBunsIsActive();
+        mainPageObj.waitLoadingTabSauces();
+        mainPageObj.clickTabSauces();
+        mainPageObj.waitLoadingTabBuns();
+        mainPageObj.checkIfTabSaucesIsActive();
+    }
+
 }
