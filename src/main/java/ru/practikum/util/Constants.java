777055ed0f6116ae64782a85cc6c65b8f9e797d8
Liubov Butorina <liubovbutorina7@yandex.ru>
2025-08-18 23:38:12 +0300

    all tasks are done

--- src/main/java/ru/practikum/util/Constants.java
+++ src/main/java/ru/practikum/util/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
     public static final String RESOURCE_URL = "https://stellarburgers.nomoreparties.site/";
     public static final String RESOURCE_URL_REGISTRATION = "https://stellarburgers.nomoreparties.site/register";
     public static final String RESOURCE_URL_LOGIN = "https://stellarburgers.nomoreparties.site/login";
-    public static final int WAITING_SECONDS = 100;
+    public static final int WAITING_SECONDS = 10;
 
     public static final String HOST = "https://stellarburgers.nomoreparties.site/";
     public static final String REGISTER_USER = "/api/auth/register";
